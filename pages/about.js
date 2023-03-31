e9a1f0dcd78fd91e3be624bc3fc0b0b493ce4f03
UserOne <menwanirwork00@gmail.com>
2023-03-31 13:14:52 +0530

    First file

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>AboutPage</h2>
+      <p> This is the app to find the latest DJ and other musical events</p>
+      <p> Version 1.0.0</p>
+    </div>
+  );
+}
